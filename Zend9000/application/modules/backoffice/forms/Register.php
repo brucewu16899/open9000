1dac5c37d5e21cd5ca8f7eecce2d1f1df6a50fb4
Louis Van de Calseyde <Louis@mbsp.local>
2013-01-03 22:04:26 +0100

    db shit

--- Zend9000/application/modules/backoffice/forms/Register.php
+++ Zend9000/application/modules/backoffice/forms/Register.php
@@ -5,9 +5,16 @@ class Backoffice_Form_Register extends Zend_Form
 
     public function init()
     {
+<<<<<<< HEAD
         /* Form Elements & Other Definitions Here ... */
+=======
+	/* Form Elements & Other Definitions Here ... */
+>>>>>>> db shit
     }
 
 
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> db shit
