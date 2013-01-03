810170287ad77e47cde57b8ca8b2db2f8b96cf7c
Pierre <pierre@yaska.eu>
2013-01-03 21:59:05 +0100

    admincontroller

--- Zend9000/application/modules/backoffice/controllers/AdminController.php
+++ Zend9000/application/modules/backoffice/controllers/AdminController.php
@@ -3,16 +3,120 @@
 class Backoffice_AdminController extends Zend_Controller_Action
 {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
+	    protected $_auth = null;
 
-    public function indexAction()
-    {
-        // action body
-    }
+	    public function init()
+	    {
+	        $this->_auth = Zend_Auth::getInstance();
+	    }
 
+	    public function indexAction()
+	    {
+	        Zend_Debug::dump($this->_auth->hasIdentity());
+	        Zend_Debug::dump($this->_auth->getIdentity());
+	    }
+
+	    public function loginAction()
+	    {
+	        $form = new Backoffice_Form_Login();
+
+	        $translate = Zend_Registry::get('Zend_Translate');
+
+	        $view = $this->view;
+	        $view->title = $translate->_('Login') . ' - Backoffice';
+
+	        $request = $this->getRequest();
+
+	        if ($request->isPost() ) {
+	            if ($form->isValid( $request->getPost() )) {
+	                $values = $form->getValues();
+
+	                $admin = new Backoffice_Model_Admin($values);
+
+	                $adapter = new Ahs_Auth_Adapter_Admin($admin->getUsername(),
+	                                                      $admin->getPassword());
+
+	                $this->_auth->authenticate($adapter);
+
+	                if ($this->_auth->hasIdentity() ) {
+	                    $admin_data = $adapter->getResultRowObject();
+
+	                    $this->_auth->getStorage()->write(array('role' => Ahs_Acl::ROLE_ADMIN,
+	                                                            'id'   => (int) $admin_data->adm_id,
+	                                              ));
+	                    return $this->redirect('backoffice/index/');
+	                } else {
+	                    // Unable to authenticate
+	                    return $this->redirect('backoffice');
+	                }
+	            }
+	        }
+	        $view->form = $form;
+	    }
+
+	    public function logoutAction()
+	    {
+	        $this->_auth->clearIdentity();
+
+	        return $this->redirect('backoffice');
+	    }
+
+	    public function registerAction()
+	    {
+	        $form = new Backoffice_Form_Register();
+
+	        $view = $this->view;
+	        $view->title = 'Registreren - Backoffice';
+
+	        $request = $this->getRequest();
+
+	        if ($request->isPost() ) {
+	            if ($form->isValid( $request->getPost() )) {
+	                $values = $form->getValues();
+					//                Zend_Debug::dump($values);
+
+	                $admin = new Backoffice_Model_Admin($values);
+
+					//                Zend_Debug::dump($admin);
+
+	                $adminMapper = new Backoffice_Model_AdminMapper();
+	                $adminMapper->save($admin);
+	            }
+	        }
+	        $view->form = $form;
+	    }
+	    
+	    public function editAction()
+	    {
+	        $form = new Backoffice_Form_Register();
+
+	        $view = $this->view;
+	        $view->title = 'Edit - Backoffice';
+
+
+	        $auth = Zend_Auth::getInstance();
+	        $id = $auth->getStorage()->read()['id']; // PHP 5.4 feature
+
+	        $adminMapper = new Backoffice_Model_AdminMapper();
+	        $array = $adminMapper->read($id);
+
+	        Zend_Debug::dump($array);
+
+	        $form->populate($array);
+
+	        $request = $this->getRequest();
+
+	        if ($request->isPost() ) {
+	            if ($form->isValid( $request->getPost() )) {
+	                $values = $form->getValues();
+
+	                $admin = new Backoffice_Model_Admin($values);
+	                $admin->setId($id);
+	                $adminMapper->save($admin);
+	            }
+	        }
+	        $view->form = $form;
+	    }
 
 }
 
--- Zend9000/application/modules/backoffice/controllers/IndexController.php
+++ Zend9000/application/modules/backoffice/controllers/IndexController.php
@@ -3,15 +3,24 @@
 class Backoffice_IndexController extends Zend_Controller_Action
 {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
+	/**
+	 * @var Zend_Auth
+	 */
+	protected $_auth;
 
-    public function indexAction()
-    {
-        // action body
-    }
+	public function init()
+	{
+	    $this->_auth = Zend_Auth::getInstance();
+	}
+
+	public function indexAction()
+	{
+	    if ($this->_auth->hasIdentity()) {
+	        $this->redirect('backoffice/admin/');
+	    } else {
+	        $this->redirect('backoffice/admin/login');
+	    }
+	}
 
 
 }
