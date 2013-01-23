02429587530f8d78e2b31ad96f2fb30da19142fd
Pierre <pierre@yaska.eu>
2013-01-23 13:10:23 +0100

    errorstyling

--- Zend9000/application/modules/backoffice/controllers/AdminController.php
+++ Zend9000/application/modules/backoffice/controllers/AdminController.php
@@ -51,8 +51,7 @@ class Backoffice_AdminController extends Zend_Controller_Action
         //Zend_Debug::dump($this->_auth->hasIdentity());
         //Zend_Debug::dump($this->_auth->getIdentity());
 
-
-        if ($this->_auth->hasIdentity() ) {
+        if ( $this->_auth->hasIdentity() ) {
             $auth = Zend_Auth::getInstance();
             $id = $auth->getStorage()->read()['id']; // PHP 5.4 feature
 
@@ -71,36 +70,40 @@ class Backoffice_AdminController extends Zend_Controller_Action
 
     public function loginAction()
     {
-        $form = new Backoffice_Form_Login();
+        if ( $this->_auth->hasIdentity() ) {
+            return $this->redirect('backoffice/admin');
+        } else {
+            $form = new Backoffice_Form_Login();
 
-        $translate = Zend_Registry::get('Zend_Translate');
+            $translate = Zend_Registry::get('Zend_Translate');
 
-        $view = $this->view;
-        $view->title = $translate->_('Login') . ' - Backoffice';
+            $view = $this->view;
+            $view->title = $translate->_('Login') . ' - Backoffice';
 
-        $request = $this->getRequest();
+            $request = $this->getRequest();
 
-        if ($request->isPost() ) {
-            if ($form->isValid( $request->getPost() )) {
-                $values = $form->getValues();
+            if ($request->isPost() ) {
+                if ($form->isValid( $request->getPost() )) {
+                    $values = $form->getValues();
 
-                $admin = new Backoffice_Model_Admin($values);
+                    $admin = new Backoffice_Model_Admin($values);
 
-                $adapter = new Ahs_Auth_Adapter_Admin($admin->getUsername(),
-                                                      $admin->getPassword());
+                    $adapter = new Ahs_Auth_Adapter_Admin($admin->getUsername(),
+                                                          $admin->getPassword());
 
-                $this->_auth->authenticate($adapter);
+                    $this->_auth->authenticate($adapter);
 
-                if ($this->_auth->hasIdentity() ) {
-                    $admin_data = $adapter->getResultRowObject();
+                    if ($this->_auth->hasIdentity() ) {
+                        $admin_data = $adapter->getResultRowObject();
 
-                    $this->_auth->getStorage()->write(array('role' => Ahs_Acl::ROLE_ADMIN,
-                                                            'id'   => (int) $admin_data->adm_id,
-                                              ));
-                    return $this->redirect('backoffice/index/');
-                } else {
-                    // Unable to authenticate
-                    return $this->redirect('backoffice');
+                        $this->_auth->getStorage()->write(array('role' => Ahs_Acl::ROLE_ADMIN,
+                                                                'id'   => (int) $admin_data->adm_id,
+                                                  ));
+                        return $this->redirect('backoffice/index/');
+                    } else {
+                        // Unable to authenticate
+                        return $this->redirect('backoffice');
+                    }
                 }
             }
         }
